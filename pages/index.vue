a6d3409b356c2661f02051c3767cd5c5e2033864
mhafet17 <do.crazy192@gmail.com>
2020-05-29 06:18:00 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-blue-500 text-xl text-center mt-5 font-bold">
+    <h1>
+      Blank Page
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
